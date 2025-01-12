db00f8a739eb9c39dd29f0bd2cc1a22f173c2e98
tanya <comiksans999@mail.ru>
2025-01-12 22:12:06 +0300

    начинаю контакты

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,17 +4,17 @@ import foto from './osnovatel 1.png'
 const Home = () => {
     return (
     <>
-        <div className={styles.roundedbox}>
-        <div className={styles.logo}>
-            <img src={foto} alt="Основатель" />
+      
+        <div className={styles.home}>
+        <div className={styles.container}>
+        <div className={styles.whiterectangle}></div>
+        <div className={`${styles.rectangle} ${styles.rectangle1}`}></div>
+        <div className={`${styles.rectangle} ${styles.rectangle2}`}></div>
         </div>
-        <div className={styles.roundedtext}>Текст</div>
-        </div>
-           
-
-        <div className={styles.deviz}>
-            <h3>Почувствуй разницу!</h3>
-            <h5>Еда, которой можно <span>доверять</span></h5>
+            <div className={styles.deviz}>
+                <h3>Почувствуй разницу!</h3>
+                <h5>Еда, которой можно <span>доверять</span></h5>
+            </div>
         </div>
 
         </>
